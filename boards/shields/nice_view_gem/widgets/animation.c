60047296d84bf8470ee85e4f1fb72ea5c821bce1
Willian Dias Tamagi <will.tamagi@gmail.com>
2024-11-16 16:22:10 -0300

    Adjust image

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -19,7 +19,7 @@ LV_IMG_DECLARE(crystal_14);
 LV_IMG_DECLARE(crystal_15);
 LV_IMG_DECLARE(crystal_16);
 
-LV_IMG_DECLARE(nview_moon);
+LV_IMG_DECLARE(moon120);
 
 const lv_img_dsc_t *anim_imgs[] = {
     &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
@@ -36,7 +36,6 @@ void draw_animation(lv_obj_t *canvas) {
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
 #else
     lv_obj_t *art = lv_img_create(canvas);
 
@@ -44,8 +43,9 @@ void draw_animation(lv_obj_t *canvas) {
     /* srand(k_uptime_get_32()); */
     /* int random_index = rand() % length; */
 
-    lv_img_set_src(art, &nview_moon);
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
+    lv_img_set_src(art, &moon120);
 #endif
+
+lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
 
 }
